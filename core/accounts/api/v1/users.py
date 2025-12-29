f38e688b021519e0a03014cc772ac897127e456a
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2025-12-29 15:04:57 +0330

    add pagination filter order and search for user list

--- core/accounts/api/v1/users.py
+++ core/accounts/api/v1/users.py
@@ -1,12 +1,24 @@
 from .serializers import UserSerializer,UserReadUpdateSerializer
 from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .pagination import LargeResultSetPagination
+
 
 class Userlist(ListCreateAPIView):
         serializer_class = UserSerializer
         queryset = User.objects.all()
+        filter_backends = [DjangoFilterBackend, SearchFilter,OrderingFilter]
+        filterset_fields = ["is_active",]
+        search_fields = ["=username","email"]
+        ordering_fields  =["date_joined"]
+        pagination_class = LargeResultSetPagination
+
+
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
         serializer_class = UserReadUpdateSerializer
         queryset = User.objects.all()
         lookup_field = "id"
+        
