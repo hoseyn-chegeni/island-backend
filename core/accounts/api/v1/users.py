d65602a4963577eb3daf9b8062ff30692d2539a0
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2025-12-29 12:46:32 +0330

    remove useless import in users.py api

--- core/accounts/api/v1/users.py
+++ core/accounts/api/v1/users.py
@@ -1,8 +1,5 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .serializers import UserSerializer,UserReadUpdateSerializer
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 class Userlist(ListCreateAPIView):
