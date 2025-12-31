86dd965ff71df52375e66d2d5dc5ba6fc4b1b727
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2025-12-31 15:43:55 +0330

    add vehicles location model

--- core/vehicles/admin.py
+++ core/vehicles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Vehicle, VehicleImage
+from .models import Vehicle, VehicleImage, VehicleLocation
 # Register your models here.
 
 
 admin.site.register(Vehicle)
 admin.site.register(VehicleImage)
+admin.site.register(VehicleLocation)
--- core/vehicles/models.py
+++ core/vehicles/models.py
@@ -30,3 +30,14 @@ class VehicleImage(models.Model):
 
     def __str__(self):
         return f"Image for Vehicle #{self.vehicle_id}"
+    
+
+
+class VehicleLocation(models.Model):
+    vehicle = models.ForeignKey("vehicles.Vehicle",on_delete=models.CASCADE,related_name="locations")
+    latitude = models.DecimalField(max_digits=9,decimal_places=6)
+    longitude = models.DecimalField(max_digits=9,decimal_places=6)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.vehicle.id} at {self.latitude}, {self.longitude}"
