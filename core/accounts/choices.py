ad5e8622e260840b4df7618d12ead7c6a3415725
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2025-12-31 09:15:20 +0330

    add vendor model

--- core/accounts/choices.py
+++ core/accounts/choices.py
@@ -4,3 +4,13 @@ from django.db import models
 class Gender(models.TextChoices):
     MALE = "M", "Male"
     FEMALE = "F", "Female"
+
+class VendorType(models.TextChoices):
+    INDIVIDUAL = "IND", "Individual"
+    COMPANY = "COM", "Company"
+
+
+class VendorStatus(models.TextChoices):
+    ACTIVE = "ACTIVE", "Active"
+    INACTIVE = "INACTIVE", "Inactive"
+    SUSPENDED = "SUSPENDED", "Suspended"
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import UserManager
-from .choices import Gender
+from .choices import Gender, VendorStatus,VendorType
 
 # Create your models here.
-
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
@@ -30,11 +29,22 @@ class Profile(models.Model):
     avatar_url = models.ImageField(blank=True, null=True)
     national_id = models.CharField(max_length=20, blank=True, null=True, unique=True)
     date_of_birth = models.DateField(blank=True, null=True)
-    gender = models.CharField(
-        max_length=1, choices=Gender.choices, blank=True, null=True
-    )
+    gender = models.CharField(max_length=1, choices=Gender.choices, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.user.email
+
+
+class Vendor(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="vendor")
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=10,choices=VendorType.choices)
+    contact_phone = models.CharField(max_length=20)
+    status = models.CharField(max_length=20,choices=VendorStatus.choices,default=VendorStatus.ACTIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
