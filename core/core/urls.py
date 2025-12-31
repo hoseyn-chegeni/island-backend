7481b0c4e1b5abf28a42caa4de31b9183c77c700
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2025-12-31 16:37:17 +0330

    add rentals app

--- core/core/urls.py
+++ core/core/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("vehicles/", include("vehicles.urls")),
+    path("rentals/",include("rentals.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
