8074b67738b5dce7a50948b1689b0eb9b5341b25
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2026-01-02 10:18:44 +0330

    add blog models

--- core/blogs/models.py
+++ core/blogs/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.title
