7b053345432a89549d401d6c1137fad331904b6e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2026-01-04 13:02:16 +0330

    rmeove check vehicle api

--- core/rentals/api/urls.py
+++ core/rentals/api/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from .v1.views import (
-    CheckVehicleAvailabilityAPIView,
     VehicleRentalDetailAPIView,
     VehicleRentalListCreateAPIView,
 )
 
 
 urlpatterns = [
-    path(
-        "vehicles/check-availability/",
-        CheckVehicleAvailabilityAPIView.as_view(),
-    ),
+
     path("vehicles/", VehicleRentalListCreateAPIView.as_view()),
     path("vehicles/<int:id>/", VehicleRentalDetailAPIView.as_view()),
 ]
