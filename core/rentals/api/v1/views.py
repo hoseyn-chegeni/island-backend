b83aa5a881984dd86bb0aba6a43341c2712695b5
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2026-01-04 13:06:35 +0330

    add permissions for rental apis

--- core/rentals/api/v1/views.py
+++ core/rentals/api/v1/views.py
@@ -1,14 +1,16 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import VehicleRentalSerializer
 from ...models import VehicleRental
-
+from rest_framework.permissions import IsAdminUser
+from core.utils import IsOwnerOrAdmin
 
 class VehicleRentalListCreateAPIView(ListCreateAPIView):
     queryset = VehicleRental.objects.all()
     serializer_class = VehicleRentalSerializer
-
+    permission_classes = [IsAdminUser,]
 
 class VehicleRentalDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = VehicleRental.objects.all()
     serializer_class = VehicleRentalSerializer
     lookup_field = "id"
+    permission_classes = [IsOwnerOrAdmin,]
