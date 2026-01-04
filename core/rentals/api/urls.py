5d791a0a0415b37d818a6602a1abbd31d3805d45
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2026-01-04 13:41:48 +0330

    add my rent history api view

--- core/rentals/api/urls.py
+++ core/rentals/api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from .v1.views import (
     VehicleRentalDetailAPIView,
     VehicleRentalListCreateAPIView,
+    MyRentHistoryAPIView,
 )
 
 
 urlpatterns = [
-
     path("vehicles/", VehicleRentalListCreateAPIView.as_view()),
     path("vehicles/<int:id>/", VehicleRentalDetailAPIView.as_view()),
+    path('my-rent-history/', MyRentHistoryAPIView.as_view()),
 ]
--- core/rentals/api/v1/views.py
+++ core/rentals/api/v1/views.py
@@ -1,8 +1,11 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from .serializers import VehicleRentalSerializer
 from ...models import VehicleRental
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
 from core.utils import IsOwnerOrAdmin
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+
 
 class VehicleRentalListCreateAPIView(ListCreateAPIView):
     queryset = VehicleRental.objects.all()
@@ -14,3 +17,24 @@ class VehicleRentalDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = VehicleRentalSerializer
     lookup_field = "id"
     permission_classes = [IsOwnerOrAdmin,]
+
+
+
+
+class MyRentHistoryAPIView(ListAPIView):
+    serializer_class = VehicleRentalSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+
+        user_id = self.request.user.id  # Get the user id from the JWT
+        return VehicleRental.objects.filter(user_id=user_id)  # Filter rentals by user_id
+
+    def list(self, request, *args, **kwargs):
+        """
+        Custom list method to handle the response format.
+        """
+        rentals = self.get_queryset()
+        serializer = self.get_serializer(rentals, many=True)
+        return Response(serializer.data)
