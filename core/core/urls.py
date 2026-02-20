012769a8405bfc5af19742379887af8b005a0cfe
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2026-02-20 11:12:12 +0330

    edit root path

--- core/core/urls.py
+++ core/core/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path(
-        "docs/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema_swagger_ui",
     ),
